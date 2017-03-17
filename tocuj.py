88e21f367bc6f54e42bc0cc653e1f0c06d2b833c
filipspl <stefaniak@gmail.com>
2017-03-17 22:07:58 +0100

    autocommit-pią, 17 mar 2017, 22:07:58 CET

---
+++ tocuj.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+fname="README.md"
+
+with open(fname) as f:
+    for line in f:
+	if re.match("^#{1,3} ", line):
+
+	    header = re.search('^(#{1,3}) ', line).group(1)
+
+	    #print "header: ", header
+	    header = header.replace("###", "  - ")
+	    header = header.replace("##", " - ")
+	    header = header.replace("#", "- ")
+
+	    name = re.sub("^#+ |\n", "", line)
+
+	    #link = name.replace(" ", "-").replace(":", "-")
+	    link = re.sub(" |:|\(|\)|\,", "-", name)
+	    link = re.sub("-+", "-", link)
+	    link = re.sub("^-|-$|\n", "", link)
+
+
+	    print "%s[%s](%s)" % (header, name, link)
